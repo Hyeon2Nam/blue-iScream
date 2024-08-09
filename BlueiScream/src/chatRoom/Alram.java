ad56d9184ff55af2f238552ffa2a83f6595c7ab8
Your Name <you@example.com>
2024-08-09 10:15:52 +0900

    add admin user block

--- BlueiScream/src/chatRoom/Alram.java
+++ BlueiScream/src/chatRoom/Alram.java
@@ -84,35 +84,29 @@ public class Alram extends JFrame {
         // JDialog 생성
         JDialog dialog = new JDialog();
         dialog.setTitle(roomName);
-        dialog.setSize(300, 150); // 원하는 크기로 설정
+        dialog.setSize(300, 150);
         dialog.setLayout(new BorderLayout());
         dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
-        // 메시지 레이블 생성
+        String m = msg;
+        if (m.length() > 10) {
+            m  = m.substring(10) + "...";
+        }
         JLabel label = new JLabel("[" + id + "] : " + msg, SwingConstants.CENTER);
         dialog.add(label, BorderLayout.CENTER);
 
-        // 화면 사이즈 가져오기
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        // 다이얼로그 사이즈 가져오기
         Dimension dialogSize = dialog.getSize();
 
-        // 우측 하단 모서리에 다이얼로그 위치 설정
         int x = screenSize.width - dialogSize.width;
         int y = screenSize.height - dialogSize.height;
         dialog.setLocation(x, y);
 
-        // 타이머 설정: duration 밀리초 후에 알림창 닫기
-        Timer timer = new Timer(duration, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();
-            }
-        });
+        Timer timer = new Timer(duration, e -> dialog.dispose());
 
-        timer.setRepeats(false); // 타이머가 한 번만 실행되도록 설정
-        timer.start(); // 타이머 시작
+        timer.setRepeats(false);
+        timer.start();
 
-        dialog.setVisible(true); // 알림창 표시
+        dialog.setVisible(true);
     }
 }
